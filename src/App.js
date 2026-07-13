d86a0a4bc01b57186c453b9b510e9d2f265669c5
Onuma Dokpikul <bp.onuma@gmail.com>
2026-07-13 11:05:46 +0700

    Update App.js with css inline

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import './App.css';
 
 function App() {
-    let r = 'React'
-    let rn = 'React Native'
-    const el = (
-      <div>
-        Create Web & Mobile App<br/>
-        with {r} & {rn}
-      </div>
-    )
-    return el
+    const divStyle = {
+      color: 'red',
+      backgroundColor: 'powderblue',
+      fontSize: 'larger',
+      padding: '3px',
+    }
+    return <div style={divStyle}>Hello React</div>
 }
-
-// function App() {
-//   return (
-//     <table border="1">
-//       <tr><th>Product</th><th>Price</th></tr>
-//       <tr><td>React</td><td>320</td></tr>
-//       <tr><td>React Native</td><td>340</td></tr>
-//     </table>
-//   )
-// }
 
 export default App;
