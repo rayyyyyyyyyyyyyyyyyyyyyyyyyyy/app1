6aafe57e6d0d337a5c1e83a6dfce491f207ad627
Onuma Dokpikul <bp.onuma@gmail.com>
2026-08-03 10:18:02 +0700

    Insert calculator.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
 import React from 'react';
-import { Header, Content, Footer } from './func-components';
-import Calender from './class-components';
+import {Calculator2} from './calculator';
 
-function App() {
-  return (
-    <>
-      <Header/>
-      <p><center><Calender/></center></p>
-      <Content/>
-      <Footer/>
-    </>
-  )
+export default function App() {
+  return <Calculator2/>
 }
-
-export default App;
