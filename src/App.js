b565fe9a43526b9cf87d61244f442990e652063d
Onuma Dokpikul <bp.onuma@gmail.com>
2026-08-24 10:45:31 +0700

    Create context, context-header2 and context-content2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
-import React from 'react';
-import Effect from './effect';
+import React, {useState} from 'react';
+import {userContext} from './context'
+import Header2 from './context-header2'
+import Content2 from './context-content2'
 
 export default function App() {
-  return <Effect/>
+  let [user, setUser] = React.useState('Tom Jerry')
+  return (
+    <userContext.Provider value={[user, setUser]}>
+      <Header2/>
+      <Content2/>
+    </userContext.Provider>
+  )
 }
